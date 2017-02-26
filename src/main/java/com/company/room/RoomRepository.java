248ac24fc20df4b658eef9e7aa6e0f3292d8b750
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-02-27 00:44:31 +0200

    Added additioanal searches to find rooms by name and less than a specified area.

--- src/main/java/com/company/room/RoomRepository.java
+++ src/main/java/com/company/room/RoomRepository.java
@@ -1,11 +1,20 @@
 package com.company.room;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 //import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface RoomRepository extends PagingAndSortingRepository<Room, Long> {
 //    @Override
 //    @PreAuthorize("hasRole('ROLE_ADMIN')")
 //    Room save(Room entity);
+    @RestResource(rel = "name-contains", path = "containsName")
+    Page<Room> findByNameContaining(@Param("name") String name, Pageable page);
+
+    @RestResource(rel = "area-less-than", path= "areaLessThan")
+    Page<Room> findByAreaLessThan(@Param("area") Integer name, Pageable page);
 }
