e1e3e34550b8624bd10314c76a35f48c3d629bb4
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-02-26 19:17:51 +0200

    Added model classes for each entity

---
+++ src/main/java/com/company/device/Device.java
@@ -0,0 +1,78 @@
+package com.company.device;
+
+import com.company.control.Control;
+import com.company.core.BaseEntity;
+import com.company.room.Room;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Device extends BaseEntity{
+    private String name;
+
+    @ManyToOne
+    private Room room;
+
+    @OneToMany(mappedBy = "device", cascade = CascadeType.ALL)
+    private List<Control> controls;
+
+    protected Device(){
+        super();
+        controls = new ArrayList<>();
+    }
+
+    public Device(String name){
+        this();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public List<Control> getControls() {
+        return controls;
+    }
+
+    public void setControls(List<Control> controls) {
+        this.controls = controls;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Device device = (Device) o;
+
+        if (!name.equals(device.name)) return false;
+        if (!room.equals(device.room)) return false;
+        return controls.equals(device.controls);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + room.hashCode();
+        result = 31 * result + controls.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/com/company/room/Room.java
@@ -0,0 +1,91 @@
+package com.company.room;
+
+import com.company.core.BaseEntity;
+import com.company.device.Device;
+import com.company.user.User;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Room extends BaseEntity{
+    private String name;
+
+    private Integer area;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List<Device> devices;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List<User> users;
+
+    protected Room(){
+        super();
+        devices = new ArrayList<>();
+        users = new ArrayList<>();
+    }
+
+    public Room(String name, Integer area){
+        this();
+        this.name = name;
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<Device> devices) {
+        this.devices = devices;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        if (!name.equals(room.name)) return false;
+        if (!area.equals(room.area)) return false;
+        if (!devices.equals(room.devices)) return false;
+        return users.equals(room.users);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + area.hashCode();
+        result = 31 * result + devices.hashCode();
+        result = 31 * result + users.hashCode();
+        return result;
+    }
+}
